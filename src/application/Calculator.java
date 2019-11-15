594935a3701a377a5a80418cf15776290a02caae
FoolishNinja <carlo.schmid1@swisscom.com>
2019-11-15 13:56:20 +0100

    Implemented first function for calculation of R

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -47,21 +47,17 @@ public class Calculator {
 
 	}
 
-	private double PFromUAndI() {
-		return voltage * current;
-	}
+	private double PFromUAndI() {return voltage * current;}
 
-	private double PFromRAndI() {
-		return resistance * Math.pow(current, 2);
-	}
+	private double PFromRAndI() {return resistance * Math.pow(current, 2);}
 	
-	private double PFromUAndR() {
-		return Math.pow(voltage, 2) / resistance;
-	}
+	private double PFromUAndR() {return Math.pow(voltage, 2) / resistance;}
 
 	private double UFromRAndI() {return resistance * current;}
 
 	private double UFromPAndI() {return power / current;}
 
 	private double UFromPAndR() {return Math.sqrt(power * resistance);}
+
+	private double RFromUAndI() {return voltage / current;}
 }
